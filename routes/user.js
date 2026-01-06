bbc8bbc3db0a03aaeecd6b5749cba31aa06ab858
Brunoooo <brunotenorio15@hotmail.com>
2026-01-06 12:31:06 -0300

    requisição patch para atualizacao de informaçoes do perfil do usuario

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
+const jwt = require('../token');
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -42,6 +44,29 @@ router.post("/login", async (req, res) => {
     userResponse.token = user.generateAuthToken();
 
     res.json(userResponse);
+})
+
+router.patch("/:id", jwt.validateToken, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+        console.log(id)
+        console.log(req.body);
+
+        const user = await User.findByIdAndUpdate(id, { $set: updates }, { new: true });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const userResponse = user.toObject();
+        delete userResponse.passwordHash;
+        userResponse.token = user.generateAuthToken();
+
+        res.json(userResponse);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating user", error: error.message });
+    }
 })
 
 module.exports = router;
