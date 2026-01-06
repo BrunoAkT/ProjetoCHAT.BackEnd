baa4436b8fdc6985294f9e68133339e9471d6e63
Brunoooo <brunotenorio15@hotmail.com>
2026-01-06 14:00:54 -0300

    Forma de patch da imagem do usuario com o cloudinary e adicao do multer

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const User = require("../models/user");
 const jwt = require('../token');
-const user = require("../models/user");
+const upload = require("../middlewares/upload");
+const cloudinary = require("../config/cloudinary");
+const streamifier = require("streamifier");
 
 const router = express.Router();
 
@@ -68,5 +70,50 @@ router.patch("/:id", jwt.validateToken, async (req, res) => {
         res.status(500).json({ message: "Error updating user", error: error.message });
     }
 })
+
+router.patch("/:id/avatar", jwt.validateToken, upload.single("avatar"), async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(req.file);
+
+        if (!req.file) {
+            return res.status(400).json({ message: "Arquivo não enviado" });
+        }
+
+        const streamUpload = () => {
+            return new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream(
+                    {
+                        folder: "profile_avatars",
+                        public_id: `avatar_${id}`,
+                        overwrite: true,
+                    },
+                    (error, result) => {
+                        if (result) resolve(result);
+                        else reject(error);
+                    }
+                );
+
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        }
+
+        const result = await streamUpload();
+
+        const user = await User.findByIdAndUpdate(
+            id,
+            { $set: { avatarUrl: result.secure_url } },
+            { new: true });
+
+        const userResponse = user.toObject();
+        delete userResponse.passwordHash;
+        userResponse.token = user.generateAuthToken();
+
+        res.json(userResponse);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating avatar", error: error.message });
+    }
+}
+)
 
 module.exports = router;
